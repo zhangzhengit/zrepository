2c514691d60eb7340fb352c6cd9130fc5c8c9f0b
zhangzhen <1071324756@qq.com>
2024-05-04 17:16:14 +0800

    整理IDE提示的代码问题

--- sb_zrepository/src/main/java/com/vo/anno/UserRepositoryTest1.java
+++ sb_zrepository/src/main/java/com/vo/anno/UserRepositoryTest1.java
@@ -15,43 +15,34 @@ import com.vo.ZRepository;
  */
 public class UserRepositoryTest1 {
 
-	public static String[] findZRSubclassFanxing(final Class cls) {
-		// FIXME 2023年6月15日 下午8:46:27 zhanghen: 去掉ccc，暂时方便测试用的
-		final Class ccc =  cls;
-
+	public static String[] findZRSubclassFanxing(final Class<?> cls) {
 		// FIXME 2023年6月17日 下午6:48:51 zhanghen: com改为配置
 //		final Set<Class<?>> c = ClassUtil.scanPackage("com");
-		final Set<Class<?>> c = ZRepositoryMain.scanPackage_COM();
-		for (final Class<?> class1 : c) {
-			if(!ccc.getCanonicalName() .equals(class1.getCanonicalName())) {
+		final Set<Class<?>> classSet = ZRepositoryMain.scanPackage_COM();
+		for (final Class<?> class1 : classSet) {
+			if (!cls.getCanonicalName().equals(class1.getCanonicalName())) {
 				continue;
 			}
 
-			final Class<?>[] is = class1.getInterfaces();
-			for (final Class<?> class2 : is) {
-				if(class2.getCanonicalName().equals(ZRepository.class.getCanonicalName())) {
-//					System.out.println("是ZR的子类,class = " + class1);
+			for (final Class<?> cls2 : class1.getInterfaces()) {
+				if(cls2.getCanonicalName().equals(ZRepository.class.getCanonicalName())) {
 					final Type[] genericInterfaces = class1.getGenericInterfaces();
 					for (final Type type : genericInterfaces) {
-//						System.out.println("\t type = " + type);
 						final String typeName = type.getTypeName();
-//						System.out.println("typeName = " + typeName);
 						final int i = typeName.indexOf("<");
 						if (i > -1) {
 							final int i2 = typeName.lastIndexOf(">");
 							if (i2 > i) {
 								final String t = typeName.substring(i + 1, i2);
-//								System.out.println("t = " + t);
-								final String[] a = t.split(",");
-//								System.out.println("a = " + Arrays.toString(a));
-
-								return a;
+								final String[] array = t.split(",");
+								return array;
 							}
 						}
 					}
 				}
 			}
 		}
+
 		return null;
 	}
 }
