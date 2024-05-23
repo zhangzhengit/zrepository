886acdb400d7f60da74936e0b8cf8f71517c75e0
zhangzhen <1071324756@qq.com>
2024-05-23 20:26:22 +0800

    由于 zframework-core 修改了Starter.start 方法，所以本工程的zf-starter也改了

---
+++ sb_zrepository/src/main/java/com/vo/starter/Test_ZFStarter.java
@@ -0,0 +1,52 @@
+package com.vo.starter;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+import com.vo.ScanPackage;
+import com.vo.configuration.ZProperties;
+import com.vo.core.ZClass;
+import com.vo.core.ZContext;
+import com.vo.core.ZLog2;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * 测试 通过 zframework.factories 指定的启动类
+ *
+ * @author zhangzhen
+ * @date 2024年2月17日
+ *
+ */
+public class Test_ZFStarter implements ZStarter {
+
+	private static final ZLog2 LOG = ZLog2.getInstance();
+
+	@Override
+	public void start() {
+
+		System.out.println(Thread.currentThread().getName() + "\t" + LocalDateTime.now() + "\t" + "Test_ZFStarter.start()");
+
+		final String scanPackageName = String
+				.valueOf(ZProperties.getInstance().getProperty("zrepository.scanPackageName"));
+		System.out.println("ZR-ZF-starter-启动--ZProperties.getInstance().getProperty");
+		if (StrUtil.isEmpty(scanPackageName)) {
+			throw new IllegalArgumentException("zrepository.scanPackageName 未配置！");
+		}
+
+		ScanPackage.set(Sets.newHashSet(scanPackageName));
+
+		final Map<Class, ZClass> clsMap = ZRepositoryStarter.startZRepository(scanPackageName);
+		final Set<Entry<Class, ZClass>> es = clsMap.entrySet();
+		for (final Entry<Class, ZClass> entry : es) {
+			LOG.info("开始注入实现类[{}]", entry.getValue().getName());
+			ZContext.addBean(entry.getKey().getClass(), entry.getValue().newInstance());
+			LOG.info("注入实现类[{}]成功", entry.getValue().getName());
+		}
+
+	}
+
+}
