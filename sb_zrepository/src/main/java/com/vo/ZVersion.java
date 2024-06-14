1aee90b14289eb4683950fc83d70dacc3f4f6a64
zhangzhen <1071324756@qq.com>
2024-06-15 06:21:44 +0800

    TODO @ZVersion  写这个功能

---
+++ sb_zrepository/src/main/java/com/vo/ZVersion.java
@@ -0,0 +1,13 @@
+package com.vo;
+
+/**
+ * 用在 @see ZEntity 标记的类里的字段上，表示此字段为乐观锁控制字段
+ *
+ * @author zhangzhen
+ * @date 2024年6月15日 上午6:20:51
+ *
+ */
+// FIXME 2024年6月15日 上午6:21:29 zhangzhen : 写这个功能
+public @interface ZVersion {
+
+}
