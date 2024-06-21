a313c1dd3cc4c2bf6c2c7531c91b7d92423df53d
zhangzhen <1071324756@qq.com>
2024-06-21 13:05:09 +0800

    1、修改ZPreventDeleteAll的javadoc 2、删除已完成的TODO

--- sb_zrepository/src/main/java/com/vo/ZPreventDeleteAll.java
+++ sb_zrepository/src/main/java/com/vo/ZPreventDeleteAll.java
@@ -7,14 +7,18 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- *	用在 @ZEntity 标记的类上，表示此类在执行 [ZRepository.deleteAll] 时，
- *	判断是否带了条件，如果不带条件则抛出异常，防止代码编写手误等而删除了全部数据
+ * 用在 @ZEntity 标记的类上，仅在执行 [ZRepository.deleteAll] 时，
+ * 此注解起作用：阻止不带条件的deleteAll，包括逻辑删除和物理删除。
+ *
+ * 即：阻止 [ZRepository.deleteAll] 操作，因为 [ZRepository.deleteAll]
+ * 就是不带任务条件的(WHERE 1 = 1这种不算)。
+ *
+ * 如果就是需要[ZRepository.deleteAll]操作，则不要使用本注解
  *
  * @author zhangzhen
  * @date 2024年6月21日 上午11:23:38
  *
  */
-// FIXME 2024年6月21日 上午11:25:16 zhangzhen : 写这个
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
