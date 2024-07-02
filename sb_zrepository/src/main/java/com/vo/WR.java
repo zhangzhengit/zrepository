b7c90ac6e139aaf078e6140de9a162df73a7c98c
zhangzhen <1071324756@qq.com>
2024-07-02 18:47:35 +0800

    WR 添加注释

--- sb_zrepository/src/main/java/com/vo/WR.java
+++ sb_zrepository/src/main/java/com/vo/WR.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * ZRWrapper 生成的结果
+ * ZRWrapper 组装所有条件后生成的结果
  *
  * @author zhangzhen
  * @date 2024年6月30日 下午2:58:25
@@ -16,13 +16,39 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class WR {
 
-	private String sql;
 	/**
-	 * 完整的where部分，包括后面的order by 和limit部分
+	 * 完整的一个整个SQL
+	 * 如：
+	 * 	SELECT * FROM user WHERE name = ? ORDER BY create_time DESC LIMIT 20 OFFSET 0;
+	 */
+	private String sql;
+
+	/**
+	 * 完整的WHERE部分，包括后面的ORDER BY 和LIMIT部分
+	 * 如：
+	 * 	WHERE name = ? ORDER BY create_time DESC LIMIT 20 OFFSET 0
 	 */
 	private String fullWhere;
+
+	/**
+	 * WHERE部分，只包括WHERE部分，条件过滤的部分
+	 * 如：
+	 * 	WHERE name = ?
+	 */
 	private String where;
+
+	/**
+	 * ORDER BY 部分
+	 * 如：
+	 * 	ORDER BY create_time DESC
+	 */
 	private String orderBy;
+
+	/**
+	 * LIMIT部分
+	 * 如：
+	 * 	LIMIT 20 OFFSET 0
+	 */
 	private String limit;
 
 }
