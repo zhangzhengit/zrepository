c6fd4c126cabc6c7770feb7281ee94c2c7c474bf
zhangzhen <1071324756@qq.com>
2024-11-26 23:07:20 +0800

    删除已完成的TODO：已经确定不支持float了

--- sb_zrepository/src/main/java/com/vo/anno/ZEntity.java
+++ sb_zrepository/src/main/java/com/vo/anno/ZEntity.java
@@ -29,12 +29,6 @@ import com.vo.conn.ZDatasourcePropertiesLoader;
  * @date 2023年6月15日
  *
  */
-// FIXME 2024年5月10日 下午10:18:36 zhangzhen: mysql float 类型 where column = value 查不出数据
-// 考虑怎么做，是限制用mysql时ZEntity不允许使用float？还是在jdbc层面处理？
-
-// FIXME 2024年5月12日 下午9:18:33 zhangzhen: float 类型查询暂用 setDouble(index,float)解决了，但是否提示出不建议mysql用float类型？
-// 建议用double或者直接用decimal？
-
 // FIXME 2024年5月19日 下午9:28:07 zhangzhen: Entity 中使用java.sql.Date 还是有问题，在save和findById时的类型转换还是有问题，尤其是pgsql
 
 @Documented
